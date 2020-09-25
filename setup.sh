e8490c66ff163b93bb6b98850b4924edf49acadc
slashexec <slashexec.dev@gmail.com>
2020-09-25 19:17:12 +0000

    Start mongo on port 27015

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 echo "Init and configure MongoDB"
 mkdir -p /workspace/data
 echo "Start Mongod WITHOUT auth"
-nohup mongod --port 27016 --dbpath /workspace/data > /dev/null 2>&1
+nohup mongod --port 27015 --dbpath /workspace/data > /dev/null 2>&1
 echo "Create MongoDB user"
-mongo admin --host localhost --eval "db.createUser({user: 'tamboyah', pwd: '_tadidi', roles: [{role: 'userAdminAnyDatabase', db: 'admin'}]});"
+mongo admin --host localhost --port 27015 --eval "db.createUser({user: 'tamboyah', pwd: '_tadidi', roles: [{role: 'userAdminAnyDatabase', db: 'admin'}]});"
 echo "Stop MongoDB"
-mongo execompte_db --host localhost --eval "db.adminCommand( { shutdown: 1 } );"
+mongo admin --host localhost --port 27015 --eval "db.adminCommand( { shutdown: 1 } );"
 #echo "Start Mongod WITH auth"
 #nohup mongod --auth --port 27017 --dbpath /workspace/data > /dev/null 2>&1
 echo "DONE: Init and configure MongoDB"
