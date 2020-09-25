fad574c1323d3b773cf9a594ea189f9ff8cf4cd3
slashexec <slashexec.dev@gmail.com>
2020-09-25 20:00:37 +0000

    add role in execompte_db

--- createMongoDbUser.sh
+++ createMongoDbUser.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Create MongoDB user"
-mongo admin --host localhost --port 27015 --eval "db.createUser({user: 'tamboyah', pwd: '_tadidi', roles: [{role: 'userAdminAnyDatabase', db: 'admin'}]});"
+
+mongo admin --host localhost --port 27015 --eval "db.createUser({user: 'tamboyah', pwd: '_tadidi', roles: [{role: 'userAdminAnyDatabase', db: 'admin'}, {role: 'userAdminAnyDatabase', db: 'execompte_db'}]});"
 echo "Stop MongoDB to apply change"
 mongo admin --host localhost --port 27015 --eval "db.adminCommand( { shutdown: 1 } );"
 echo "STOPPED MongoDB to apply change"
