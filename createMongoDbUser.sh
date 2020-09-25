80f1dfe766b0b5dd62f8f26f81a1e476983a25da
slashexec <slashexec.dev@gmail.com>
2020-09-25 21:12:25 +0000

    adding readWriteAnyDatabase

--- createMongoDbUser.sh
+++ createMongoDbUser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Create MongoDB user"
-
-mongo admin --host localhost --port 27015 --eval "db.createUser({user: 'tamboyah', pwd: '_tadidi', roles: [{role: 'userAdminAnyDatabase', db: 'admin'}, {role: 'userAdminAnyDatabase', db: 'execompte_db'}]});"
+#mongo admin --host localhost --port 27015 --eval "use execompte_db;"
+mongo admin --host localhost --port 27015 --eval "db.createUser({user: 'tamboyah', pwd: '_tadidi', roles: [{role: 'userAdminAnyDatabase', db: 'admin'}, , 'readWriteAnyDatabase']});"
 echo "Stop MongoDB to apply change"
 mongo admin --host localhost --port 27015 --eval "db.adminCommand( { shutdown: 1 } );"
 echo "STOPPED MongoDB to apply change"
